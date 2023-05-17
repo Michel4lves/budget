80f36cde572090f0e676e768a261a6b0646f429a
michel4alves <michelsantosa@gmail.com>
2023-05-17 00:02:54 -0300

    add contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,16 @@
+import styles from './pagesCss/Contact.module.css'
+
 export default function Contact() {
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className={styles.contact_container}>
+            <h1>Gostou?</h1>
+            <h1>Têm alguma ideia?</h1>
+            <h1>Vamos fazer juntos!</h1>
+            <p>Se você gostou deste sistema... <br/>
+            entre em contato! <br/>
+            Traga suas ideias para que possamos juntos dar vida a mais um projeto.</p>
+            <p>Se quiser ver mais, veja meus repositórios no <span>Github</span> ou visite meu perfil no <span>Linkedin</span> através dos links no rodapé desta página.</p>
+            <p>Obrigado por apreciar meu trabalho!</p>
         </div>
     )
 }
