d72cb254a4809ed9ff79aade5030cc23f5fcf8c8
michel4alves <michelsantosa@gmail.com>
2023-05-16 23:12:06 -0300

    Add responsive mode

--- src/components/service/serviceCard.js
+++ src/components/service/serviceCard.js
@@ -9,13 +9,13 @@ export default function ServiceCard({ id, name, cost, description, handleRemove 
     }
 
     return (
-        <div className={styles.project_card}>
+        <div className={styles.service_card}>
             <h4>{name}</h4>
             <p>
                 <span>Custo total:</span> R${cost}
             </p>
             <p>{description}</p>
-            <div className={styles.project_card_actions}>
+            <div className={styles.service_card_actions}>
                 <button onClick={remove}>
                     <BsFillTrashFill />
                     Excluir
