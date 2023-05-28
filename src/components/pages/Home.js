213582c7b0c0c4c2e58cd7672aa235c818ea7a42
michel4alves <michelsantosa@gmail.com>
2023-05-28 02:30:57 -0300

    fixing add empty storage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,16 @@ import styles from './pagesCss/Home.module.css'
 import banner from '../../img/calculator.png'
 import LinkButton from '../layout/Buttons/LinkButton'
 
+import internaldb from '../../internaldb.json'
+
 export default function Home() {
+
+    
+    if (!localStorage.hasOwnProperty('projects')) {
+        localStorage.setItem('projects', JSON.stringify(internaldb.projects))
+    }
+
+
     return (
         <section className={styles.home_container} >
             <h1>Bem vindo ao <span>Budget</span></h1>
